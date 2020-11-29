fab118361c87825020ef49bf3e71d4ce91147bc5
Jiachen Bai <646329483@qq.com>
2020-11-28 21:18:26 -0800

    added update content table feature to populate_db

--- src/utils/populate_db.js
+++ src/utils/populate_db.js
@@ -10,7 +10,7 @@ const CONTENT_MYSQL_DB_PASSWORD = constants.CONTENT_MYSQL_DB_PASSWORD;
 const CONTENT_MYSQL_DB_DATABASE_NAME = constants.CONTENT_MYSQL_DB_DATABASE_NAME;
 const CONTENT_MYSQL_DB_TABLE_NAME = constants.CONTENT_MYSQL_DB_TABLE_NAME;
 
-const Content = require("../models/content_model");
+const Content = require("../content/models/content_model");
 
 // Initiate mysql client
 const mysql = require("mysql2");
@@ -23,6 +23,19 @@ const dbConn = mysql.createConnection({
   database: CONTENT_MYSQL_DB_DATABASE_NAME
 });
 
+/* width by height */
+const resolutionMap = {
+  1: [1600, 899],
+  2: [640, 638],
+  3: [972, 1300],
+  4: [1000, 684],
+  5: [2991, 1682],
+  6: [600, 400],
+  7: [450, 450],
+  8: [550, 738],
+  9: [450, 300],
+  10: [720, 720]
+}
 
 const newContent = new Content({
   creater: 1,
@@ -35,17 +48,52 @@ const newContent = new Content({
   length: 54
 });
 
-dbConn.connect(function(err) {
-  if (err) throw err;
-  for (var i = 0; i < 1000; i++) {
-    const query = `INSERT INTO ${CONTENT_MYSQL_DB_TABLE_NAME} SET ? `;
-    dbConn.query(query, newContent, function(err, res) {
-      if (err) {
-        console.log(query);
-        console.log(err);
-      } else {
-        console.log(res.insertId);
-      }
-    });
-  };
-});
+function insertIntoDB() {
+  dbConn.connect(function(err) {
+    if (err) throw err;
+    for (var i = 0; i < 5000; i++) {
+      const query = `INSERT INTO ${CONTENT_MYSQL_DB_TABLE_NAME} SET ? `;
+      dbConn.query(query, newContent, function(err, res) {
+        if (err) {
+          console.log(query);
+          console.log(err);
+        } else {
+          console.log(res.insertId);
+        }
+      });
+    };
+  });
+}
+
+/* min is included, max is not */
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function updateDB() {
+  dbConn.connect(function(err) {
+    if (err) throw err;
+    for (var i = 0; i < 5000; i++) {
+      const index = getRandomInt(1, 11)
+      const w = resolutionMap[index][0]
+      const h = resolutionMap[index][1]
+      const query = `UPDATE ${CONTENT_MYSQL_DB_TABLE_NAME} SET \
+                     thumbUrl="sample_${index}.jpg", \
+                     width=${w}, \
+                     height=${h} \
+                     WHERE id=${i+1} `;
+      dbConn.query(query, function(err, res) {
+        if (err) {
+          console.log(query);
+          console.log(err);
+        } else {
+          console.log(res.insertId);
+        }
+      });
+    };
+  });
+}
+
+updateDB()
